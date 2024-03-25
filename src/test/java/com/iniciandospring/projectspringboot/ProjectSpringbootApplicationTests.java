17f543f7bc873c6d38807b7f6237e1c0dcdc10b5
Dickson Freitas <dickfreitas12@gmaill.com>
2024-03-25 11:41:47 -0300

    Project created

---
+++ src/test/java/com/iniciandospring/projectspringboot/ProjectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iniciandospring.projectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
