f1fad4b5c93c9f4cafd03e8c5b64c7a08b58fedd
Dickson Freitas <dickfreitas12@gmaill.com>
2024-03-27 10:48:50 -0300

    Service layer and component registration

--- src/main/java/com/iniciandospring/projectspringboot/repositories/UserRepository.java
+++ src/main/java/com/iniciandospring/projectspringboot/repositories/UserRepository.java
@@ -2,8 +2,13 @@ package com.iniciandospring.projectspringboot.repositories;
 
 import com.iniciandospring.projectspringboot.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 //JPA REPOSITORY É UM TIPO GENERICS ONDE PEDE 2 CHAVES A CLASSE QUE IRA USAR E O TIPO DO ID, NESSE CASO O LONG
+/*NESSA CLASSE FICA A CRITERIO DA PESSOA COLOCAR O @REPOSITORY(QUE TEM A MESMA IDEIA DO @SERVICE, NA CAMADA DE SERVIÇO)
+POIS JPAREPOSITORY JA FAZ ESSE TRABALHO AUTOMATICAMENTE POR BAIXO DOS PANOS,
+*/
+@Repository
 public interface UserRepository extends JpaRepository<User , Long> {
 
 }
---
+++ src/main/java/com/iniciandospring/projectspringboot/services/UserService.java
@@ -0,0 +1,36 @@
+package com.iniciandospring.projectspringboot.services;
+
+
+import com.iniciandospring.projectspringboot.entities.User;
+import com.iniciandospring.projectspringboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/*O @SERVICE REGISTRA A CLASSE COMO UM COMPONENTE SPRING
+PODENDO ASSIM SER INJETADO AUTOMATICAMENTE EM OUTRA CLASSE COM O
+AUTOWIRED
+EXISTE OUTROS 2 @COMPONENT E @REPOSITORY, QUE TEM O MSM CONTEXTO
+SO SAO MAIS SEMANTICOS
+*/
+@Service
+public class UserService {
+    //INJEÇÃO DE DEPENDENCIAS
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return  repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return  obj.get();
+
+    }
+
+
+}
