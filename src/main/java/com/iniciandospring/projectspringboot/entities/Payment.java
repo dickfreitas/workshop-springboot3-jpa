ea50f1685e9b1dc431eb9df47849e71cbc831820
Dickson Freitas <dickfreitas12@gmaill.com>
2024-03-28 16:14:59 -0300

    Implements Total e subTotal

--- src/main/java/com/iniciandospring/projectspringboot/entities/Payment.java
+++ src/main/java/com/iniciandospring/projectspringboot/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.iniciandospring.projectspringboot.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -13,7 +14,7 @@ public class Payment implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant date;
-
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
